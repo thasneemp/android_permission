ac5a7261691dcdafeb6b5ef5c0de808026666503
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2018-06-02 23:46:17 +0400

    permission implemented

--- app/src/main/java/app/example/com/androidpermission/MainActivity.java
+++ app/src/main/java/app/example/com/androidpermission/MainActivity.java
@@ -1,13 +1,124 @@
 package app.example.com.androidpermission;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getName();
+    private static final String[] PERMISSIONS_READ_WRITE = {Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int REQUEST_PERMISSION_STORAGE = 101;
+    AppCompatButton mReadContactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mReadContactButton = findViewById(R.id.readContact);
+        mReadContactButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Contacts permissions have not been granted.
+            Log.i(TAG, "Storage permissions has NOT been granted. Requesting permissions.");
+            requestStoragePermissions();
+
+        } else {
+            // Contact permissions have been granted. Show the contacts fragment.
+            Log.i(TAG,
+                    "Storage permissions have already been granted. Displaying contact details.");
+            writeToStorage();
+        }
+    }
+
+    private void writeToStorage() {
+        if (isExternalStorageWritable()) {
+            getPrivateAlbumStorageDir(this, "testPermission");
+        }
+    }
+
+    public File getPrivateAlbumStorageDir(Context context, String albumName) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + File.separator + "test.txt");
+        try {
+            if (file.createNewFile()) {
+                Log.e(TAG, "File  created");
+            } else {
+                Log.e(TAG, "File not created");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    private void requestStoragePermissions() {
+        // BEGIN_INCLUDE(contacts_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            ActivityCompat.requestPermissions(this, PERMISSIONS_READ_WRITE, REQUEST_PERMISSION_STORAGE);
+        } else {
+            // Contact permissions have not been granted yet. Request them directly.
+            ActivityCompat.requestPermissions(this, PERMISSIONS_READ_WRITE, REQUEST_PERMISSION_STORAGE);
+        }
+        // END_INCLUDE(contacts_permission_request)
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION_STORAGE) {
+            Log.i(TAG, "Received response for contact permissions request.");
+            // We have requested multiple permissions for contacts, so all of them need to be
+            // checked.
+            if (PermissionUtil.verifyPermissions(grantResults)) {
+                // All required permissions have been granted, display contacts fragment.
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.i(TAG, "Contacts permissions were NOT granted.");
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
